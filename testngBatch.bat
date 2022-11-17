3808741e02191a307215dff096d5c54d3613a014
Вадим Клементьев <klementevv@quantumart.ru>
2022-11-17 15:19:00 +0300

    init repo

---
+++ testngBatch.bat
@@ -0,0 +1,5 @@
+set projectLocation=E:\Projects\Autotest\Maven\Pochtabank2
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
