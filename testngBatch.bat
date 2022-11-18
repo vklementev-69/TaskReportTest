2459780e533950cef7268f6aeb7195357dc320a6
Вадим Клементьев <klementevv@quantumart.ru>
2022-11-18 10:08:30 +0300

    refactoring

--- testngBatch.bat
+++ testngBatch.bat
@@ -1,5 +1,5 @@
-set projectLocation=E:\Projects\Autotest\Maven\Pochtabank2
+set projectLocation=E:\Projects\Autotest\Maven\TaskReportTest
 cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*;%projectLocation%\src\test\java;
 java org.testng.TestNG %projectLocation%\testng.xml
 pause
